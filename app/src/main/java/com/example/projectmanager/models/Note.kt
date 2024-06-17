f465bea52394672fbb31cb9a4c9e4d909be2c659
ishan420 <kingerishan@gmail.com>
2024-06-17 13:46:37 +0530

    Setting up Room Database for Note

--- app/src/main/java/com/example/projectmanager/models/Note.kt
+++ app/src/main/java/com/example/projectmanager/models/Note.kt
@@ -1,4 +1,16 @@
 package com.example.projectmanager.models
 
-class Note {
-}
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+
+@Entity(tableName = "notes")
+@Parcelize
+data class Note(
+    @PrimaryKey(autoGenerate = true)
+    val id : Int,
+    val noteTitle : String,
+    val noteDesc : String
+) : Parcelable
--- app/src/main/java/com/example/projectmanager/roomdatabase/NoteDatabase.kt
+++ app/src/main/java/com/example/projectmanager/roomdatabase/NoteDatabase.kt
@@ -1,4 +1,34 @@
 package com.example.projectmanager.roomdatabase
 
-class NoteDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.projectmanager.models.Note
+
+
+@Database(entities = [Note::class], version = 1, exportSchema = false)
+abstract class NoteDatabase : RoomDatabase() {
+
+    abstract val noteDao: NoteDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: NoteDatabase? = null
+
+        fun getInstance(context: Context): NoteDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        NoteDatabase::class.java,
+                        "note_db"
+                    ).fallbackToDestructiveMigration().build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
 }
